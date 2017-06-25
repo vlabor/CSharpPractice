7551b17154182264fd20a534196564f1580d86d1
vlabor <v.labor@gmail.com>
2017-06-25 01:01:01 -0400

    added bootstrap optimization
    added bootstrap optimization

---
+++ CSharp.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using CSharp.Data;
+using CSharp.Infrastructure;
+using CSharp.Repositories;
+using CSharp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharp.Web.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            SetAutofacContainer();
+        }
+
+        private static void SetAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(AwardRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+            // Services
+            builder.RegisterAssemblyTypes(typeof(AwardService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
